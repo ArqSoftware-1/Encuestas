26fb4cc3e4faa079e36481ab985e2bc699712962
Fernando Di Meglio <dimegliofernando@gmail.com>
2016-10-26 19:27:12 -0300

    Refactor

--- client/components/shared/titulo.component.ts
+++ client/components/shared/titulo.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "titulo",
-    template: `<h2 class="ui header divided">{{nombre}}</h2>`
+    template: `<h2>{{nombre}}</h2>`
 })
 export class TituloComponent {
     @Input() nombre: string;
