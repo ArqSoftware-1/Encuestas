7d072cb94af3abde84a9a458bec1e2963db228f7
José Di Meglio <jgdimeglio@gmail.com>
2016-11-07 23:37:35 -0300

    Arreglos

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,4 @@
-/// <reference path="../node_modules/typescript/lib/lib.es6.d.ts" />
+/// <reference path="../node_modules/typescript/lib/lib.es5.d.ts" />
 import * as express from "express";
 import { join } from "path";
 import * as favicon from "serve-favicon";
