d8ca5af76aaa9178ed2438ab7e21572ac28c7254
unknown <jgdimeglio@gmail.com>
2016-11-12 17:31:08 +0100

    Arreglo de error

--- server/models/Director.ts
+++ server/models/Director.ts
@@ -3,12 +3,12 @@ import { Document, Schema, model } from 'mongoose'
 class Director {
     email: String;
     password: String;
-    salt: String;
+    salt: Buffer;
 }
 
 var EsquemaDirector = new Schema({ email: { type: String }, 
                                    password: { type: String},
-                                   salt: { type: String}
+                                   salt: { type: Buffer}
                                  });
 
 interface DocumentoDirector extends Director, Document { };
