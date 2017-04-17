c4bc82926e9d6216499f88e59eaf9540da07bacc
José Di Meglio <jgdimeglio@gmail.com>
2017-04-17 01:46:25 -0300

    Test de respuestas encuesta

--- test/respuestasEncuesta.js
+++ test/respuestasEncuesta.js
@@ -8,7 +8,11 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = "http://localhost:3000";
 let should = chai.should();
+
+let RespuestaEncuesta = require("../server/models/RespuestaEncuesta");
 let token;
+let respuestaEncuesta;
+let encuesta;
 
 chai.use(chaiHttp);
 
@@ -23,13 +27,8 @@ describe('RespuestasEncuesta sin autenticacion', () => {
     });
 
     it('Deberia responder con error 403 si se solicita crear una nueva respuesta de encuesta', (done) => {
-        encuesta = {anho: 2018,
-                    semestre: 1,
-                    carrera: 'Licenciatura en informática',
-                    fechaLimite: '2017/09/23'};
         chai.request(server)
             .post('/api/respuestas/encuesta/guardar')
-            .send({encuesta: encuesta})
             .end((err, res) => {
                 res.should.have.status(403);
                 done();
@@ -37,8 +36,9 @@ describe('RespuestasEncuesta sin autenticacion', () => {
     });
 });
 
+/*
 describe('RespuestasEncuesta con autenticacion', () => {
-    /*before(function(done){
+    before(function(done){
         let credenciales = {
             email: "director@unq.edu.ar",
             password: "1234"
@@ -47,11 +47,31 @@ describe('RespuestasEncuesta con autenticacion', () => {
             .post('/login')
             .send(credenciales)
             .end((err, res) => {
-                var result = JSON.parse(res.text);
-                token = result.jwt;
-                done();
+                token = JSON.parse(res.text).jwt;
+
+                RespuestaEncuesta.ModeloRespuestaEncuesta.findOne({
+                    dni: '12345678'
+                }).exec()
+                .then(respuestaEncuestaRes => {
+                    respuestaEncuesta = respuestaEncuestaRes;
+                    done();
+                })
         });
 
-     });*/
+     });
+
+    it('Deberia devolver la respuesta de encuesta correcta cuando se la busca por el id', (done) => {
+        chai.request(server)
+            .post('/api/publica/respuestas/encuesta/detalle')
+            .send({token: respuestaEncuesta.token})
+            .set('Authorization', token)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+               // res.body.should.have.property('anho').eql(encuesta.anho);
+                done();
+            });
+    });
 
 });
+*/
