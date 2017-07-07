9c52bd843110b5c40abd4d0f48a80f8e8e06011c
José Di Meglio <jgdimeglio@gmail.com>
2017-07-07 17:26:59 -0300

    Evitando duplicacion de datos

--- server/dataTest.ts
+++ server/dataTest.ts
@@ -20,39 +20,47 @@ var objectsToSave = [];
 var opcion1 = new ModeloOpcion({
     descripcion: "Voy a cursar en c1",
     limite: 2,
-    tipo: NombresOpcionDefecto.tipos.comision
+    tipo: NombresOpcionDefecto.tipos.comision,
+    _id: crearId(1)
 });
 var opcion2 = new ModeloOpcion({
     descripcion: "Voy a cursar en c2",
     limite: 1,
-    tipo: NombresOpcionDefecto.tipos.comision
+    tipo: NombresOpcionDefecto.tipos.comision,
+    _id: crearId(2)
 });
 var opcion3 = new ModeloOpcion({
     descripcion: "Voy a cursar en c3",
     limite: 3,
-    tipo: NombresOpcionDefecto.tipos.comision
+    tipo: NombresOpcionDefecto.tipos.comision,
+    _id: crearId(3)
 });
 var opcion4 = new ModeloOpcion({
     descripcion: NombresOpcionDefecto.descripciones.ya_curse,
-    tipo: NombresOpcionDefecto.tipos.ya_curse
+    tipo: NombresOpcionDefecto.tipos.ya_curse,
+    _id: crearId(4)
 });
 var opcion5 = new ModeloOpcion({
     descripcion: NombresOpcionDefecto.descripciones.me_gustaria_pero_no_puedo,
-    tipo: NombresOpcionDefecto.tipos.no_voy_a_cursar
+    tipo: NombresOpcionDefecto.tipos.no_voy_a_cursar,
+    _id: crearId(5)
 });
 var opcion6 = new ModeloOpcion({
     descripcion: NombresOpcionDefecto.descripciones.no_voy_a_cursar,
-    tipo: NombresOpcionDefecto.tipos.no_voy_a_cursar
+    tipo: NombresOpcionDefecto.tipos.no_voy_a_cursar,
+    _id: crearId(6)
 });
 var opcion7 = new ModeloOpcion({
     descripcion: "Voy a cursar en c4",
     limite: 3,
-    tipo: NombresOpcionDefecto.tipos.comision
+    tipo: NombresOpcionDefecto.tipos.comision,
+    _id: crearId(7)
 });
 var opcion8 = new ModeloOpcion({
     descripcion: "Voy a cursar en c5",
     limite: 3,
-    tipo: NombresOpcionDefecto.tipos.comision
+    tipo: NombresOpcionDefecto.tipos.comision,
+    _id: crearId(8)
 });
 
 
@@ -70,25 +78,29 @@ var materia1 = new ModeloMateria({
     opciones: [opcion1, opcion2, opcion4, opcion5, opcion6],
     nombre: "Introducción a la programación",
     codigo: "Intr",
-    idOpcionPorDefecto: opcion4._id
+    idOpcionPorDefecto: opcion4._id,
+    _id: crearId(100)
 });
 var materia2 = new ModeloMateria({
     opciones: [opcion1, opcion4, opcion5, opcion6],
     nombre: "Organización de las computadores",
     codigo: "Orga",
-    idOpcionPorDefecto: opcion4._id
+    idOpcionPorDefecto: opcion4._id,
+    _id: crearId(101)
 });
 var materia3 = new ModeloMateria({
     opciones: [opcion1, opcion4, opcion5, opcion6],
     nombre: "Matemática 1",
     codigo: "Mate1",
-    idOpcionPorDefecto: opcion4._id
+    idOpcionPorDefecto: opcion4._id,
+    _id: crearId(102)
 });
 var materia4 = new ModeloMateria({
     opciones: [opcion1, opcion3, opcion4, opcion5, opcion6],
     nombre: "Programación funcional",
     codigo: "pf",
-    idOpcionPorDefecto: opcion4._id
+    idOpcionPorDefecto: opcion4._id,
+    _id: crearId(103)
 });
 
 objectsToSave.push(materia1);
@@ -103,7 +115,8 @@ for(var i = 0; i < 50; i++){
         opciones: [opcion1, opcion3, opcion4, opcion5, opcion6],
         nombre: "Materia " + i,
         codigo: "Mat" + i,
-        idOpcionPorDefecto: opcion4._id
+        idOpcionPorDefecto: opcion4._id,
+        _id: crearId(i)
     });
     objectsToSave.push(materia);
     materiasParaEncuesta.push(materia);
@@ -116,7 +129,8 @@ var encuesta = new ModeloEncuesta({
     semestre: 2,
     carrera: 'Licenciatura en informática',
     fechaLimite: new Date(2017, 11, 31),
-    estaActiva: true
+    estaActiva: true,
+    _id: crearId(1)
 });
 
 objectsToSave.push(encuesta);
@@ -127,8 +141,9 @@ var encuesta1 = new ModeloEncuesta({
     semestre: 1,
     carrera: 'Licenciatura en informática',
     fechaLimite: new Date(2017, 6, 18),
-    estaActiva: false
-});
+    estaActiva: false,
+    _id: crearId(2)
+});1
 
 objectsToSave.push(encuesta1);
 
@@ -155,7 +170,8 @@ var respuestaEncuesta1 = new ModeloRespuestaEncuesta({
     encuesta: encuesta,
     nombreYApellidoAlumno: 'Alumno',
     DNIAlumno: '12345678',
-    emailAlumno: 'alumno@unq.edu.ar'
+    emailAlumno: 'alumno@unq.edu.ar',
+    _id: crearId(100000)
 });
 respuestaEncuesta1.save();
 respuestaEncuesta1.token = respuestaEncuesta1._id + randomBytes(16).toString("hex");
@@ -182,7 +198,8 @@ pbkdf2('1234', salt, 10000, length, digest, (err: Error, hash: Buffer) => {
     var director = new ModeloDirector({
         email: 'director@unq.edu.ar',
         password: hash.toString("hex"),
-        salt: salt
+        salt: salt,
+        _id: crearId(1)
     });
     objectsToSave.push(director);
 });
